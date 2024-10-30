46e6f28370f39690fac93940f06c1a8ed4f8591c
kamangir <arash@kamangir.net>
2024-10-29 18:58:27 -0700

    test refactor - kamangir/bolt#746

--- abcli/.abcli/tests/help.sh
+++ abcli/.abcli/tests/help.sh
@@ -6,10 +6,28 @@ function test_abcli_help() {
     local module
     for module in \
         "abcli" \
-        "abcli pytest"; do
+        \
+        "abcli git" \
+        "abcli git browse" \
+        "abcli git checkout" \
+        "abcli git clone" \
+        "abcli git create_branch" \
+        "abcli git create_pull_request" \
+        "abcli git get_branch" \
+        "abcli git get_repo_name" \
+        "abcli git increment_version" \
+        "abcli git pull" \
+        "abcli git push" \
+        "abcli git recreate_ssh" \
+        "abcli git reset" \
+        "abcli git review" \
+        "abcli git status" \
+        "abcli git sync_fork"; do
         abcli_eval ,$options \
-            $module help
+            abcli_help $module
         [[ $? -ne 0 ]] && return 1
+
+        abcli_hr
     done
 
     return 0
--- abcli/__init__.py
+++ abcli/__init__.py
@@ -6,7 +6,7 @@ ICON = "🪄"
 
 DESCRIPTION = f"{ICON} a language to speak AI."
 
-VERSION = "9.370.1"
+VERSION = "9.371.1"
 
 REPO_NAME = "awesome-bash-cli"
 
