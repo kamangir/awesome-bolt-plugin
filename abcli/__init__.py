abd356576c83077c49a2a8a177913c0f4ff62caa
kamangir <arash@kamangir.net>
2024-10-31 20:26:36 -0700

    @help refactor - kamangir/bolt#746

--- abcli/__init__.py
+++ abcli/__init__.py
@@ -6,7 +6,7 @@ ICON = "🪄"
 
 DESCRIPTION = f"{ICON} a language to speak AI."
 
-VERSION = "9.377.1"
+VERSION = "9.378.1"
 
 REPO_NAME = "awesome-bash-cli"
 
--- abcli/help/functions.py
+++ abcli/help/functions.py
@@ -4,11 +4,13 @@ from blue_options.terminal import show_usage, xtra
 
 from abcli.help.git import help_functions as help_git
 from abcli.help.log import help_functions as help_log
+from abcli.help.notebooks import help_functions as help_notebooks
 from abcli.help.pytest import help_pytest
 
 
 help_functions = {
     "git": help_git,
     "log": help_log,
+    "notebooks": help_notebooks,
     "pytest": help_pytest,
 }
